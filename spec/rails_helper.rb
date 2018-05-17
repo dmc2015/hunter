8ffba303db85d9f8723db412340ffa6a9e7dbfd4
Don McLamb <mclamb.donald@gmail.com>
2018-05-16 23:54:21 -0400

    commented simplecov from rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,10 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-require 'simplecov'
-SimpleCov.start
+# require 'simplecov'
+# SimpleCov.start
 require 'rubygems'
 require 'vcr'
 require 'spec_helper'
+require 'capybara/rails'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
